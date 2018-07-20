17c4a184647261323491d06cb3978f0fffdc54e4
Diana Herran <dianam.@MacBook-Air-de-Diana.local>
2018-07-20 18:11:06 -0500

    create form add new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,25 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post= Post.new
   end
 
   def create
+    @post= Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: 'succesfully created'
+    else
+      render :new
+    end
   end
 
   def show
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:description, :location)
+  end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'posts#index'
   
-  get 'posts/index'
+  get 'posts', to: 'posts#index'
   get 'posts/new'
   get 'posts/create'
   get 'posts/show'
+
+  post 'posts', to: 'posts#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
