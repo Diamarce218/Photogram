a07cbefe8b5990bc8db652aee1aabc280053bc88
Diana Herran <dianam.@MacBook-Air-de-Diana.local>
2018-07-29 19:04:22 -0500

    add devise and User Auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'posts#index'
   
   get 'posts', to: 'posts#index'
   get 'posts/new'
-  get 'posts/show'
-  get 'posts/:id', to: 'posts#show'
-
+  get 'posts/:id', to: 'posts#show', as: 'posts_show'
+  
   post 'posts', to: 'posts#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
