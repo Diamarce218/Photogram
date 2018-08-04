d62db8392e7db64d7b72dc2407ae62d1bcc44f46
Diana Herran <dianam.@MacBook-Air-de-Diana.local>
2018-08-03 19:00:31 -0500

    add Post model test

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    username { Faker::Internet.user_name }
+    firstname { Faker::Name.name }
+    lastname { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+  end
+end 
