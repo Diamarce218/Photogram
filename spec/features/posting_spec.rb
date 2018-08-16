c382465b862468c20ffd3730458942aafc86b0e9
Diana Herran <dianam.@MacBook-Air-de-Diana.local>
2018-08-16 15:46:42 -0500

    Chages n Posting test

--- spec/features/posting_spec.rb
+++ spec/features/posting_spec.rb
@@ -1,30 +1,25 @@
 require "rails_helper"
-
  describe "the signin process" do
   before :each do
     @user = create(:user, password: 'rickandmorty')
   end
-   it "sposting something" do
-
+   it "creating post" do
     visit '/users/sign_in'
     within("#new_user") do
       fill_in 'Email', with: @user.email
       fill_in 'Password', with: 'rickandmorty'
     end
     click_button 'Log in'
-    expect(page).to have_content 'Signed in successfully'
-
     visit '/posts/new'
     within("#new_post") do
-      attach_file('post_photo', "#{Rails.root}/spec/factories/cat.jpeg")
-      fill_in 'Description', with: 'holi'
+      page.attach_file('post_photo','spec/factories/wolf.jpg')
+      fill_in 'Description', with: 'Toma lo tuyo'
+      fill_in 'Location', with: 'Toma lo tuyo'
     end
     click_button 'Submit'
-    expect(page).to have_content 'It was sucessfull'
-    @var= Post.first.description
-    expect(var).to eq('holi')
- 
+   
+    expect(page).to have_content 'The post was successfully created'
+     visit '/posts'
+    expect(page).to have_selector(:css, "div")
   end
-end
-
-
+end 
